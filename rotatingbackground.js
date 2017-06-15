3895ad6a32c6e8c66a9a9f117490313e04982528
Conner May <connerkid200022@gmail.com>
2017-06-15 12:48:19 -0400

    Create rotatingbackground.js

---
+++ rotatingbackground.js
@@ -0,0 +1,26 @@
+(function() {
+  backgrounds = [
+    "https://www.dropbox.com/s/f40upjzs5jtd2yv/thumb-1920-701999.jpg?dl=1"
+  ];
+
+  var currentIndex = getRandomInt(0, backgrounds.length - 1)
+  var previousIndex = currentIndex;
+
+  function switchBackground() {
+    if (backgrounds.length > 1) {
+      while (currentIndex == previousIndex) {
+      currentIndex = getRandomInt(0, backgrounds.length - 1);
+      }
+      previousIndex = currentIndex;
+      var url = backgrounds[currentIndex];
+      document.getElementsByClassName('app')[0].style.backgroundImage = 'url(' + url + ')';
+    }
+  }
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  setInterval(switchBackground, 600000);
+  switchBackground();
+})()
