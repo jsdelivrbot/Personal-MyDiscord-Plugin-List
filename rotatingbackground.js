5c62712cb713e4a152514cba2a85b936b6de7a90
Conner May <connerkid200022@gmail.com>
2017-06-15 14:14:15 -0400

    Create rotatingbackground.js

--- rotatingbackground.js
+++ rotatingbackground.js
@@ -1,9 +1,7 @@
 (function() {
   backgrounds = [
     "http://static.zerochan.net/Yatogami.Kuroh.full.1359059.jpg",
-    "http://cdn.wallpapersafari.com/55/61/fIcg4P.jpg",
-    "http://fungyung.com/data/out/61/62839689-k-project-wallpapers.jpg",
-    "http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1301/21/c0/17582218_1358742073462.jpg",
+    "http://fungyung.com/data/out/61/62870137-k-project-wallpapers.jpg",
   ];
 
   var currentIndex = getRandomInt(0, backgrounds.length - 1)
