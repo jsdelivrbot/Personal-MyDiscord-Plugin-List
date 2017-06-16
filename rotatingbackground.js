b4661deaab8e4f4583a3a4df31da7095896116ed
Conner May <connerkid200022@gmail.com>
2017-06-15 21:17:19 -0400

    Update rotatingbackground.js

--- rotatingbackground.js
+++ rotatingbackground.js
@@ -1,6 +1,8 @@
 (function() {
   backgrounds = [
-    "http://wallpaper-gallery.net/images/k-anime-wallpaper/k-anime-wallpaper-22.jpg",
+    "http://i.imgur.com/JuMdLBB.jpg",
+    "http://i.imgur.com/nFrvLMO.jpg",
+    "https://i.imgur.com/fIkaKnK.jpg",
   ];
 
   var currentIndex = getRandomInt(0, backgrounds.length - 1)
